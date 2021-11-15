db5c71fecf2109951a1f1071556e9e5117ff67b7
Danil Tarnovskiy <tarnovskiy2002@gmail.com>
2021-11-15 20:36:03 +0200

    Added tft lib

--- Arduino Mega modules/Display/src/main.cpp
+++ Arduino Mega modules/Display/src/main.cpp
@@ -1,7 +1,21 @@
 #include <Arduino.h>
+#include <TFT.h>
+
+#define cs   10
+#define dc   9
+#define rst  8
+
+TFT TFTscreen = TFT(cs, dc, rst);
 
 void setup() 
 {
+  TFTscreen.begin();
+  TFTscreen.background(0, 0, 0);
+  TFTscreen.stroke(255,255,255);
+  TFTscreen.setTextSize(2);
+  TFTscreen.text("Test", 0, 0);
+  TFTscreen.setTextSize(5);
+
   Serial.begin(9600);
   Serial.print("test");
 }
