d4943bb42ee88416750cb347be73d05bc4c6073e
fun-banana <tarnovskiy2002@gmail.com>
2021-12-21 17:59:19 +0200

    added Infra red detection

--- Arduino Mega modules/Blue_led_and_gass_detector/src/main.cpp
+++ Arduino Mega modules/Blue_led_and_gass_detector/src/main.cpp
@@ -1,19 +1,31 @@
 #include <Arduino.h>
 
 // VARIABLES
-int BLUE_LED = 9;
-int RELAY_GAS = 10; 
+int INFRA_RED_OUT = 8; 
+int BLUE_LED      = 9;
+int RELAY_GAS     = 10;
 
 void setup() 
 {
+  pinMode(INFRA_RED_OUT, OUTPUT); // set pin 8 as output
   pinMode(BLUE_LED, OUTPUT); // set pin 9 as output 
   pinMode(RELAY_GAS, INPUT); // set pin 10 as input
   Serial.begin(9600); // open serial port
 }
 
 void loop() 
-{
-  Serial.println(digitalRead(RELAY_GAS)); // write to serial port Relay gas status (0 \ 1)
+{ 
+  int relayGasData = digitalRead(RELAY_GAS);
 
-  digitalWrite(BLUE_LED, digitalRead(RELAY_GAS)); // turn on LED when Relay gas (pin 10) get 1 logic
+  if (relayGasData == 0)
+  {
+    digitalWrite(BLUE_LED, 1); 
+    digitalWrite(INFRA_RED_OUT, 1);
+  }
+  else 
+  {
+    digitalWrite(BLUE_LED, 0);
+  }
+
+  Serial.println(relayGasData);
 }
