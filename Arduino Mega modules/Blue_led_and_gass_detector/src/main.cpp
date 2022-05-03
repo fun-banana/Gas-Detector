ae8bd4124b0271c3732fb7b101d20d600a437d7f
fun-banana <tarnovskiy2002@gmail.com>
2022-05-03 14:11:31 +0300

    Clear code

--- Arduino Mega modules/Blue_led_and_gass_detector/src/main.cpp
+++ Arduino Mega modules/Blue_led_and_gass_detector/src/main.cpp
@@ -18,14 +18,8 @@ const int stepSpeed = 60;
 
 // VARS
 Stepper stepMotor(stepPerRevolution, step_IN1, step_IN2, step_IN3, step_IN4);
-<<<<<<< Updated upstream
-String ack = "";
-SoftwareSerial radio(radio_rx, radio_tx);
-char inputData = '0';
-=======
 Logger Log;
 int data;
->>>>>>> Stashed changes
 
 void turn_off(Stepper motor)
 {
@@ -56,41 +50,15 @@ void setup()
   stepMotor.setSpeed(stepSpeed);
   turn_left(stepMotor);
 
-<<<<<<< Updated upstream
-  Serial.begin(38400);
-  Serial1.begin(38400); // open serial port
-  radio.begin(2400); // open radio port
-=======
   Serial.begin(9600); // open serial port
   Serial2.begin(9600); // open serial radio port
   Serial3.begin(9600); // open serial bluetooth port
->>>>>>> Stashed changes
 }
 
 void loop() 
 { 
   int relayGasData = digitalRead(RELAY_GAS);
 
-<<<<<<< Updated upstream
-  if ( Serial1.available() )   {  Serial.write( Serial1.read() );  }
-
-  while (Serial1.available() > 0)
-  {
-    inputData = Serial1.read();
-    if (inputData != '0')
-    {
-      Serial.println(inputData);
-      inputData = '0';
-    }
-  }
-  
-
-  // while (radio.available())
-  // {
-  //   ack = radio.readString();
-  //   Serial.println(ack);
-  // }
-=======
   while (Serial3.available() > 0)
   {
     Log.Log(Serial3.readString());
@@ -100,7 +68,6 @@ void loop()
   {
     Serial.println(data);
   }
->>>>>>> Stashed changes
 
   if (relayGasData == 0)
   {
@@ -115,15 +82,8 @@ void loop()
     digitalWrite(INFRA_RED_OUT, 0);
 
     turn_right(stepMotor);
-
-<<<<<<< Updated upstream
-    radio.println("Hello, World!");
   }
 
-  // Serial.println(relayGasData);
-=======
     Serial3.write("WARNING");
     Serial2.write("WARNING");
-  }
->>>>>>> Stashed changes
 }
